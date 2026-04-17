c083d0075322df90df292960ab64df1f689350ad
Vishesh369 <vishesh99.8@hotmail.com>
2026-04-18 03:00:01 +0530

    24BCY70145 Vishesh

--- lab_mst/lab.sql
+++ lab_mst/lab.sql
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 SELECT Department.DeptName, COUNT(Employee.EmpID) AS TotalEmployees
 FROM Department
 LEFT JOIN Employee
 ON Department.DeptID = Employee.DeptID
 GROUP BY Department.DeptName;
+ 
+=======
+SELECT Department.DeptName, COUNT(Employee.EmpID) AS TotalEmployees
+FROM Department
+LEFT JOIN Employee
+ON Department.DeptID = Employee.DeptID
+GROUP BY Department.DeptName;
+
